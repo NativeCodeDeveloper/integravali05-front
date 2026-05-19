35511fc52cd1a80a64ccc34cdfc6c17dabc6d185
NativeCode <tm.nicolasmachuca@gmail.com>
2026-05-19 15:32:11 -0400

    asdasd1

---
+++ src/lib/publicContact.js
@@ -0,0 +1,8 @@
+export const publicContact = {
+  companyName: "SaludB",
+  address: "Region Metropolitana",
+  phone: "+56 9 8527 8325",
+  email: "Contacto@saludb.cl",
+  instagram: "@saludb.cl",
+  website: "https://www.saludb.cl",
+};
